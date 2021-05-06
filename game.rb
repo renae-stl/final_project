8f7dbffa7a0e6e5489e529d584b2ce285d08bca7
richLSb <rigorous@live.co.uk>
2021-05-06 12:24:07 +0100

    tree_ruby2d

---
+++ game.rb
@@ -0,0 +1,24 @@
+require 'ruby2d'
+
+set title: "Speed tiles!"
+
+Triangle.new(
+  x1: 320, y1: 50,
+  x2: 540, y2: 430,
+  x3: 100, y3: 430,
+  color: ['red', 'green', 'blue']
+)
+
+show
+
+# class Board
+#   def print_status
+      
+#   end
+
+#   def print_board
+      
+#   end
+
+
+# end
