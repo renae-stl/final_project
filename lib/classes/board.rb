1cd6c07f55655d3477be9665f80263bf7e14b2d5
Justin Hylton <justin.hylton.dev@gmail.com>
2021-05-07 08:44:12 +0100

    Engine Design

---
+++ lib/classes/board.rb
@@ -0,0 +1,11 @@
+class Board
+
+  def initialize
+    @board = 2
+  end
+
+  def board_print()
+    puts 'Board'
+  end
+
+end
---
+++ lib/classes/target_board.rb
@@ -0,0 +1,11 @@
+class TargetBoard
+
+  def initialize
+    @board = 3
+  end
+
+  def target_board_print()
+    puts 'Target Board'
+  end
+
+end
