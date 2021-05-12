2c780aa8b6ddd9c79c3c840459c84f663f35c0a3
richLSb <rigorous@live.co.uk>
2021-05-12 21:33:28 +0100

    game music adjustment

--- lib/slate_sliders/run.rb
+++ lib/slate_sliders/run.rb
@@ -11,9 +11,10 @@ set( {
     :background => 'gray',
 } )
 
-music = Music.new('/Users/richardlawrence/Downloads/Kid_Icarus_Uprising_Black_Feathers_in_the_Sky_OC_ReMix.mp3')
-music.play
-music.volume = 30
+music = Music.new('./resources/music/Kid_Icarus_Uprising_Black_Feathers_in_the_Sky_OC_ReMix.mp3')
+  music.play
+  music.volume = 30
+  music.loop = true
 
 
 
