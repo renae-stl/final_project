5dc0a59d59d18f35828c9dd665f6ea26e7402494
Renaé St Louis <renaestlouis@Renaes-MacBook-Pro.local>
2021-05-13 11:45:03 +0100

    start screen added

--- lib/slate_sliders/run.rb
+++ lib/slate_sliders/run.rb
@@ -5,82 +5,110 @@ require_relative 'classes/slates'
 require_relative 'classes/game'
 
 slate_sliders = Board.new
-
 set( { 
     :title => 'Slate Sliders!' ,
-    :background => 'gray',
+    :background => 'orange',
     :width => 600,
     :height => 600,
 } )
+@game_started = false
+mouse_coordinate_x = Window.mouse_x
+mouse_coordinate_y = Window.mouse_y
 
-music = Music.new('./resources/music/Kid_Icarus_Uprising_Black_Feathers_in_the_Sky_OC_ReMix.mp3')
-music.play
-music.volume = 0
+Image.new(
+    "./resources/images/SlateSliders.png",
+    x: 150, y: 0,
+    width: 300, height: 300,
+)
 
-slate_sliders.start_screen
-puts
-puts "** Startup Game Board **\n\n"
-pp slate_sliders.startup_game_board
-puts
-puts "** Print Game Board **\n\n"
-slate_sliders.print_game_board
-puts
-puts "** Inner Game Board **\n\n"
-#pp slate_sliders.inner_game_board
-
-# puts
-# puts "** SOLUTION BOARD **\n\n"
-
-#slate_sliders.timer
-
-tick = 0
-
-slate_sliders.inner_game_board
+Image.new(
+    "./resources/images/play_button.png",
+    x: 200, y: 350,
+    width: 200, height: 100,
+)
 
 
-update do
-  if tick % 60 == 0
+on :mouse_down do
+    if Window.mouse_x.between?(200, 400) && Window.mouse_y.between?(350, 450)
+        @game_started = true
+        Window.clear
+        puts "game started = #{@game_started}"
+        set( { 
+        :background => 'green',
+    } )
+
+
+    music = Music.new('./resources/music/Kid_Icarus_Uprising_Black_Feathers_in_the_Sky_OC_ReMix.mp3')
+    music.play
+    music.volume = 30
+
+    slate_sliders.start_screen
+    puts
+    puts "** Startup Game Board **\n\n"
+    pp slate_sliders.startup_game_board
+    puts
+    puts "** Print Game Board **\n\n"
     slate_sliders.print_game_board
-    slate_sliders.current_game_board
-  end
-  tick += 1
+    puts
+    puts "** Inner Game Board **\n\n"
+    #pp slate_sliders.inner_game_board
+
+    # puts
+    # puts "** SOLUTION BOARD **\n\n"
+
+    #slate_sliders.timer
+
+    tick = 0
+
+    slate_sliders.inner_game_board
+
+
+    update do
+    if tick % 60 == 0
+        slate_sliders.print_game_board
+        slate_sliders.current_game_board
+    end
+    tick += 1
+    end
+
+    on :key_down do |event|
+        if event.key == 's'
+        slate_sliders.make_move('up')
+        clear
+        #slate_sliders.inner_game_board
+        slate_sliders.print_game_board
+        end
+
+        if event.key == 'w'
+            slate_sliders.make_move('down')
+            clear
+            #slate_sliders.inner_game_board
+            slate_sliders.print_game_board
+        end
+
+        if event.key == 'd'
+            slate_sliders.make_move('left')
+            clear
+            #slate_sliders.inner_game_board
+            slate_sliders.print_game_board
+        end
+
+        if event.key == 'a'
+            slate_sliders.make_move('right')
+            clear
+            #slate_sliders.inner_game_board
+            slate_sliders.print_game_board
+        end
+
+
+    end
+
+
+    puts
+    puts "** Current Position Of Blank Slate **\n\n"
+    # pp slate_sliders.blank_slate_position
+    end
 end
 
-on :key_down do |event|
-    if event.key == 's'
-      slate_sliders.make_move('up')
-      clear
-      #slate_sliders.inner_game_board
-      slate_sliders.print_game_board
-    end
-
-    if event.key == 'w'
-        slate_sliders.make_move('down')
-        clear
-        #slate_sliders.inner_game_board
-        slate_sliders.print_game_board
-    end
-
-    if event.key == 'd'
-        slate_sliders.make_move('left')
-        clear
-        #slate_sliders.inner_game_board
-        slate_sliders.print_game_board
-    end
-
-    if event.key == 'a'
-        slate_sliders.make_move('right')
-        clear
-        #slate_sliders.inner_game_board
-        slate_sliders.print_game_board
-    end
-
-
-end
-
-
-puts
-puts "** Current Position Of Blank Slate **\n\n"
-# pp slate_sliders.blank_slate_position
 
 show
